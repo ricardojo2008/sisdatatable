979fc89b1b6e1bb2b89e32d1798fb623501a0b40
ricardojo2008 <ricardojo2008@gmail.com>
2020-03-26 22:39:34 -0300

    data table version 1.0.0

--- php/conec.php
+++ php/conec.php
@@ -1,9 +1,9 @@
 <?php 
 $sql_details = array(
-	'user' => 'rjlope2',
-	'pass' => 'M4pfre2022',
-	'dbInstance'   => '//10.223.240.160:1521/TW_MAB'
+	'user' => '*',
+	'pass' => '*',
+	'dbInstance'   => '*'
 );
-$db_test = '(DESCRIPTION=( ADDRESS_LIST= (ADDRESS= (PROTOCOL=TCP) (HOST=10.223.240.160) (PORT=1521)))( CONNECT_DATA= (SID=TW_MAB) ))';
-$conn = oci_connect("rjlope2", "M4pfre2022", $db_test);
+$db_test = '(DESCRIPTION=( ADDRESS_LIST= (ADDRESS= (PROTOCOL=TCP) (HOST=*) (PORT=*)))( CONNECT_DATA= (SID=*) ))';
+$conn = oci_connect("*", "*", $db_test);
 ?>
